8002ff8186a1b151b342d94e608f9b02642c0b35
PLGLegend <76423167+ryanthomas2005@users.noreply.github.com>
2022-01-20 17:23:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+var hr,mn,sec;
+var hrAngle,mnAngle,secAngle;
+
+function setup() {
+  createCanvas(400,400);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(0);
+  
+  translate(200,200)
+  rotate(-90)
+
+  hr = hour();
+  mn = minute();
+  sec = second();
+ 
+secAngle = map(sec,0,60,0,360);
+hrAngle = map(hr % 12,0,12,0,360);
+mnAngle = map(mn,0,60,0,360);
+
+
+push();
+rotate(secAngle);
+stroke(255,0,0);
+strokeWeight(7);
+line(0,0,100,0);
+pop();
+
+push();
+rotate(mnAngle);
+stroke(0,255,0);
+strokeWeight(7);
+line(0,0,75,0);
+pop();
+
+push();
+rotate(hrAngle);
+stroke(0,0,255);
+strokeWeight(7);
+line(0,0,50,0);
+pop();
+
+stroke(255,0,255);
+point(0,0);
+
+strokeWeight(10);
+nofill();
+
+stroke(255,0,0);
+arc(0,0,300,300,0,secAngle);
+
+stroke(0,255,0);
+arc(0,0,280,280,0,mnAngle);
+
+stroke(0,0,255);
+arc(0,0,260,260,0,hrAngle);
+
+  
+}
